6cce60dcd8c7f875c887f05ee9b986f0b5026cd1
Tun Khine <tunkhine126@gmail.com>
2019-01-29 01:54:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-if (string ===string.toLowerCase())
+if (string === string.toLowerCase())
   return "I can't hear you!";
+  else if (sting === string.toUpperCase())
+  return "YES INDEED";
+  else if (string === "I love you, Grandma.")
+  return "I love you, too."
 }
